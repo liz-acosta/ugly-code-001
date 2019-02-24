f760001465c988418a33b8cfb5a50c54cb6a6840
liz-acosta <lacosta@50-1-86-163.dsl.dynamic.fusionbroadband.com>
2019-02-24 15:30:56 -0800

    Fixed some broken things -- ANNE IS A GENIUS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask import Flask, request, redirect, url_for, render_template, jsonify, session
 import jinja2
 import csv
 import random
 from random import shuffle
+from flask.ext.session import Session
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
@@ -12,23 +13,18 @@ from wtforms import Form, BooleanField, TextField, validators
 
 from twilio.rest import Client
 
-account_sid = 'nah-uh'
-auth_token = 'nope'
+account_sid = 'nope'
+auth_token = 'nah-up'
 client = Client(account_sid, auth_token)
     
-
 TAROT_DECK_DICT = {}
 
 with open("static/20190223-chees-pixel-tarot.tsv") as tsv:
 	for line in csv.reader(tsv, dialect="excel-tab"):
 		TAROT_DECK_DICT[line[0]] = line[1:]
 
-final_cards = random.sample(xrange(78),3)
-tarot_reading = []
-for card in final_cards:
-	tarot_reading.append(TAROT_DECK_DICT[str(card)])
-
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route("/")
 def home():
@@ -38,29 +34,29 @@ def home():
 def cut_deck():
 	return render_template("cut-deck.html")
 
-@app.route("/final_reading", methods=['GET', 'POST'])
+@app.route("/final_reading")
 def final_five():
-	# text = request.form['number']
+	final_cards = random.sample(xrange(78),3)
+	tarot_reading = []
+	for card in final_cards:
+		tarot_reading.append(TAROT_DECK_DICT[str(card)])
 
-	# five_card_image = "static/53c0cce811d9a_thumb900.jpg"
+	session['reading'] = tarot_reading
+
 	return render_template("final-reading.html", tarot_reading=tarot_reading)
-
-
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     flash('Login requested for user {}, remember_me={}'.format(
-    #         form.username.data, form.remember_me.data))
-    #     return redirect('/index')
 
 @app.route("/handle_form", methods=['GET', 'POST'])
 def my_form_post():
     text = request.form['number']
-    print("*****", tarot_reading)
     processed_text = text.upper()
+    
+    tarot_reading = session.get('reading', None)
+
     tarot_reading_text = ''
     
-    for card in tarot_reading:
-    	tarot_reading_text += card[1] +'; '
+    for card in tarot_reading[:2]:
+    	tarot_reading_text += card[1] +'; '   	
+	tarot_reading_text += tarot_reading[2][1]
     	
     message = client.messages.create(body=tarot_reading_text, from_='4153001499', to=text)
 
